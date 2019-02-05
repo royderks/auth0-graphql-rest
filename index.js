a418ae4d0fb8cb4644d2bd68d810ac80be5f8555
royderks <hello@hackteam.io>
2019-02-05 00:42:16 +0000

    Added Auth0 middleware for JWT tokens, created prettier command in package.json

--- index.js
+++ index.js
@@ -1,56 +1,77 @@
-const express = require("express");
-const { ApolloServer, gql } = require("apollo-server-express");
-const fetch = require("node-fetch");
+const express = require('express')
+const { ApolloServer, gql } = require('apollo-server-express')
 
-const typeDefs = require("./typeDefs.js");
-const ProductsAPI = require("./products.js");
+const jwt = require('express-jwt')
+const jwks = require('jwks-rsa')
+require('dotenv').config()
+
+const typeDefs = require('./typeDefs.js')
+const ProductsAPI = require('./products.js')
+
+const jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+  }),
+  credentialsRequired: false,
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ['RS256'],
+})
 
 const resolvers = {
   Query: {
     products: (_, args, context) => {
-      const { dataSources } = context;
-      return dataSources.productsAPI.getProducts();
+      const { dataSources } = context
+      return dataSources.productsAPI.getProducts()
     },
     product: (parent, args, context) => {
-      const { dataSources } = context;
-      const { id } = args;
-      return dataSources.productsAPI.getProduct(id);
-    }
+      const { dataSources } = context
+      const { id } = args
+      return dataSources.productsAPI.getProduct(id)
+    },
   },
   Mutation: {
     addOffer: (parent, args, context) => {
-      const { dataSources } = context;
-      const { productId, reseller, price } = args;
-      return dataSources.productsAPI.addProductOffer(productId, reseller, price);
-    }
+      const { dataSources } = context
+      const { productId, reseller, price } = args
+      return dataSources.productsAPI.addProductOffer(productId, reseller, price)
+    },
   },
   Product: {
     reviews: (parent, args, context) => {
-      const { dataSources } = context;
-      const { id } = parent;
-      return dataSources.productsAPI.getProductReviews(id);
+      const { dataSources } = context
+      const { id } = parent
+      return dataSources.productsAPI.getProductReviews(id)
     },
     offers: (parent, args, context) => {
-      const { dataSources } = context;
-      const { id } = parent;
-      return dataSources.productsAPI.getProductOffers(id);
-    }
-  }
-};
+      const { dataSources } = context
+      const { id } = parent
+      return dataSources.productsAPI.getProductOffers(id)
+    },
+  },
+}
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   dataSources: () => ({
-    productsAPI: new ProductsAPI
-  })
-});
+    productsAPI: new ProductsAPI(),
+  }),
+  context: ({ req }) => ({
+    user: req.user,
+    token: req.headers.authorization,
+  }),
+})
 
-const app = express();
-server.applyMiddleware({ app });
+const app = express()
+app.use(jwtCheck)
+server.applyMiddleware({ app })
 
-const port = 4000;
+const port = 4000
 
-app.listen({ port }, () =>{
-  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
-});
+app.listen({ port }, () => {
+  console.log(
+    `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
+  )
+})
--- products.js
+++ products.js
@@ -1,39 +1,40 @@
-const { RESTDataSource } = require("apollo-datasource-rest");
-// const fetch = require("node-fetch");
+const { RESTDataSource } = require('apollo-datasource-rest')
 
 class ProductsAPI extends RESTDataSource {
   constructor() {
-    super();
-    this.baseURL = 'http://demo9458524.mockable.io';
+    super()
+    this.baseURL = 'http://demo9458524.mockable.io'
+  }
+
+  willSendRequest(request) {
+    request.headers.set('Authorization', this.context.token)
   }
 
   async getProducts() {
-    return await this.get(`products`);
-    //return await fetch(`${this.baseURL}/products`).then(res => res.json());
+    return await this.get(`products`)
   }
 
   async getProduct(id) {
-    return await this.get(`products/${id}`);
+    return await this.get(`products/${id}`)
   }
 
   async getProductReviews(productId) {
-    return await this.get(`products/${productId}/reviews`);
+    return await this.get(`products/${productId}/reviews`)
   }
 
   async getProductOffers(productId) {
-    return await this.get(`products/${productId}/offers`);
+    return await this.get(`products/${productId}/offers`)
   }
 
   async addProductOffer(productId, reseller, price) {
-    return await this.post(
-      `products/${productId}/offers`,
-      {
+    if (this.context.user) {
+      return await this.post(`products/${productId}/offers`, {
         productId,
         reseller,
-        price
-      }
-    )
+        price,
+      })
+    }
   }
 }
 
-module.exports = ProductsAPI;
+module.exports = ProductsAPI
